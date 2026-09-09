eb7e5be2f2845b6f5a89bd244971cabfcfa32dca
Muhammad Iezwan <anakc216@gmail.com>
2026-09-09 16:42:15 +0800

    Fix deployed referral links

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'infinity-chat-shell-v6';
+const CACHE_NAME = 'infinity-chat-shell-v7';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
