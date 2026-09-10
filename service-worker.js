2aec3f6f78798637d88ea81aad41040251f6b388
Muhammad Iezwan <anakc216@gmail.com>
2026-09-10 21:01:35 +0800

    Update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'infinity-chat-shell-v7';
+const CACHE_NAME = 'infinity-chat-shell-v8';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
